81a7be14b77b91504f3d81847754ea11c1b15f82
lefthandedpony <168099432+lefthandedpony@users.noreply.github.com>
2026-04-12 14:15:31 +0200

    Update song_similarity_app.py

--- song_similarity_app.py
+++ song_similarity_app.py
@@ -35,7 +35,7 @@ pairs = load_pairs()
 st.title("Umfrage: Vergleich von Songtexten – Ähnlichkeitsbewertung Dauer 10-20 Minuten")
 
 st.write(
-    "Erstellt von Steve Tacke. Bitte bewerte nur die inhaltliche bzw. semantische Ähnlichkeit der Texte. "
+    "Erstellt von Steve Tacke. Bitte bewerten Sie nur die inhaltliche bzw. semantische Ähnlichkeit der Texte. "
     "Melodie, Genre oder Bekanntheit der Songs sollen keine Rolle spielen."
 )
 st.header("Hinweise zur Bewertung")
